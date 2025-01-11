b984923a0e574fc289deb72105eb532c2b06f8c6
siddhantrawat402 <siddhantrawat003@gmail.com>
2025-01-11 15:38:54 +0530

    create header (add Navbar to the website)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -6,10 +5,12 @@ import Signup from './pages/Signup'
 import Signin from './pages/Signin'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes> 
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
